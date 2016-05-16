23acdd073df3cffda804f0b91cc617df6d52b66d
Paolo Barcaro <Paolo@MacBook-Pro-di-Paolo.local>
2016-05-16 23:32:25 +0200

    Added API

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -28,7 +28,9 @@ class ShowsController < ApplicationController
          	(0..((@seatsString.length - 3) / 3)).each do |i|
           	seatString = @seatsString.slice(3 * i, 3 * (i + 1))
           	row = seatString.slice(0, 1)
-          	number = seatString.slice(1,2)
+            p row
+          	number = seatString.slice(1, 2)
+            p number
           	seat = @show.theater.seats.where(row: row).where(number: number).take
           	if seat.reservations.where(show: show).exists?
           		alreadyReserved = true
@@ -61,9 +63,38 @@ class ShowsController < ApplicationController
   def api_reserve_seats
     @user = User.find_by_email(params[:email])
     if @user && @user.valid_password?(params[:password])
-      render json: @user, status: :ok
+      @seatsString = params[:seats]
+      @show = Show.find(params[:id])
+      if @seatsString && @seatsString != "" && @seatsString.length % 3 == 0
+        alreadyReserved = false
+        if DateTime.now > @show.datetime - 30.minutes && !current_user.admin
+          render nothing: true, status: :bad_request
+        else
+          reservation = Reservation.new(user: @user, show: @show)
+          (0..((@seatsString.length - 3) / 3)).each do |i|
+            seatString = @seatsString.slice(3 * i, 3 * (i + 1))
+            row = seatString.slice(0, 1)
+            p row
+            number = seatString.slice(1, 2)
+            p number
+            seat = @show.theater.seats.where(row: row).where(number: number).take
+            if seat.reservations.where(show: @show).exists?
+              alreadyReserved = true
+            end
+            reservation.seats << [seat]
+            reservation.save
+          end
+          if alreadyReserved
+            render nothing: true, status: :bad_request
+          else
+            render json: reservation, stauts: :ok
+          end
+        end
+      else 
+        render nothing: true, status: :bad_request
+      end
     elsif
-      render json: @user, status: :ok
+      render nothing: true, status: :unauthorized
     end
   end
 end
